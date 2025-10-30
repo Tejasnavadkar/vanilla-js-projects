6ae2173166a797635cf98dcf19321583bb98cd14
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-10-30 08:50:27 +0530

    how js in browser

---
+++ Part-4-Js-For-Placements/Advance_topics_&_Architecture_thinking/how_js_works_in_browser/script.js
@@ -0,0 +1,5 @@
+// call stack (Execution stack)
+
+// js single-threaded hai -> ek time pe ek hi kaam karata hai
+// jab tum function call karte ho -> wo stack ke top pe chala jata hai
+// function complete hone ke baad stack se nikal jaata hai (pop ho jata hai)
